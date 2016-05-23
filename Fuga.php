e5de652194b5009b98f50139d44e99c0a38116a1
PC277 <PC277@kabushikikaishaaidakku-no-MacBook-Pro.local>
2016-05-24 02:13:05 +0900

    first commit

---
+++ Fuga.php
@@ -0,0 +1,14 @@
+<?PHP
+/**
+ * @category Fuga
+ * @author   mkurone
+ *
+ */
+class Fuga {
+    public function index(){
+        return true;
+    }
+    public function index2() {
+        return false;
+    }
+}
